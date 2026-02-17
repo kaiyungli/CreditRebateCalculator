41debfaf6a8ae70b6c91289124fe42a8be3e744a
root <dummyegggumlei@gmail.com>
2026-02-17 16:33:13 +0000

    Fix: correct import path to ../../../lib/db

--- src/pages/api/merchant-rates/index.js
+++ src/pages/api/merchant-rates/index.js
@@ -1,4 +1,4 @@
-import { getMerchantRates } from '../../lib/db'
+import { getMerchantRates } from '../../../lib/db'
 
 export default async function handler(req, res) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'GET only' })
